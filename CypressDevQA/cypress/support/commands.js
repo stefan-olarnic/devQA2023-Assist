1842a75440734c683796827383b391bbd33565cb
Zethyne <stefanolarnic@gmail.com>
2023-04-11 21:10:27 +0300

    updated support files

--- CypressDevQA/cypress/support/commands.js
+++ CypressDevQA/cypress/support/commands.js
@@ -1,6 +1,4 @@
 import {
-  LOGIN_NR_TEL,
-  LOGIN_PASSWORD,
   CONTACT_FIRST_NAME,
   CONTACT_LAST_NAME,
   CONTACT_PHONE_NUMBER,
@@ -12,22 +10,11 @@ import {
   ADRESS,
 } from "./constants";
 
-
-
-Cypress.Commands.add("validLogin", () => {
-  cy.get('input[name="phone_number"]').type(LOGIN_NR_TEL);
-  cy.get('input[name="password"]').type(LOGIN_PASSWORD);
-  cy.get('button[type="submit"]').click();
-  cy.wait(2000);
-});
-
-  // ...
   Cypress.Commands.add("completeForm", () => {
     cy.get('input[name="contact_first_name"]').type(CONTACT_FIRST_NAME);
     cy.get('input[name="contact_last_name"]').type(CONTACT_LAST_NAME);
     cy.get('input[name="contact_phone_number"]').type(CONTACT_PHONE_NUMBER);
     cy.get(".vs__search").click();
-    //cy.wait(1000);
     cy.contains("Alimente").click();
     cy.get("textarea").type(DESCRIPTION);
     cy.get(".pac-target-input").type(ADRESS);
@@ -41,7 +28,6 @@ Cypress.Commands.add("validLogin", () => {
     cy.get('input[name="city"]').type(CITY);
     cy.get('input[name="postal_code"]').type(POSTAL_CODE);
     cy.get('.btn-primary').click()
-    //cy.wait(200);
   })
 
   Cypress.Commands.add("completeFormWithoutDescription", () => {
@@ -62,6 +48,3 @@ Cypress.Commands.add("validLogin", () => {
     cy.get('input[name="postal_code"]').type(POSTAL_CODE);
     cy.get(".btn-primary").click();
   })
-
-//cy.get("#__BVID__23").type(LOGIN_NR_TEL);
-//cy.get("#__BVID__25").type(LOGIN_PASSWORD);
--- CypressDevQA/cypress/support/constants.js
+++ CypressDevQA/cypress/support/constants.js
@@ -13,8 +13,6 @@ export const secondAccountCredentials = {
     password: "aaa"
 }
 
-export const LOGIN_NR_TEL = '0751415238'
-export const LOGIN_PASSWORD = "Cristi12345@";
 export const CONTACT_FIRST_NAME = "zzn";
 export const CONTACT_LAST_NAME = "vvx";
 export const CONTACT_PHONE_NUMBER = "0733333334";
@@ -24,6 +22,4 @@ export const CITY = "Suceava";
 export const POSTAL_CODE = "720062";
 export const DESCRIPTION = "yaby";
 export const ADRESS = "xyz";
-export const SECOND_NR_TEL ="071";
-export const SECOND_PASSWORD = "aaa"
 export const COMMENT = "random comment"
